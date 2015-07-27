a1a576515ee0ea89bb14365d706168709c415d61
mauriciolobo <mauriciolobo@gmail.com>
2015-07-26 23:54:23 -0300

    First Test Working

---
+++ AttributeFactory.Tests/CreatingNewClassTests.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AttributeFactory.Tests
+{
+    [TestFixture]
+    public class CreatingNewClassTests
+    {
+        [Test]
+        public void ShouldCreateBasicClass()
+        {
+            var factoryName = "Basic";
+
+            var result = Create(factoryName);
+
+            result.Should().BeAssignableTo<BasicClass>();
+        }
+
+        private object Create(string factoryName)
+        {
+            var typesWithMyAttribute =
+                from a in AppDomain.CurrentDomain.GetAssemblies()
+                from t in a.GetTypes()
+                from att in t.GetCustomAttributes(typeof(AbstractFactoryAttribute), false).Cast<AbstractFactoryAttribute>()
+                where att.ClassName == factoryName
+                select t;
+
+            var withMyAttribute = typesWithMyAttribute as Type[] ?? typesWithMyAttribute.ToArray();
+            if(withMyAttribute.Count()!=1)
+                throw new Exception("Cannot find classe");
+
+            var obj = Activator.CreateInstance(withMyAttribute.First());
+
+            return obj;
+        }
+    }
+
+    [AbstractFactory("Basic")]
+    public class BasicClass
+    {
+
+    }
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+    internal sealed class AbstractFactoryAttribute : Attribute
+    {
+        public string ClassName { get; private set; }
+
+        public AbstractFactoryAttribute(string className)
+        {
+            ClassName = className;
+        }
+    }
+}
